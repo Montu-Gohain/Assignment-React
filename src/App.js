728a43f2f063decc0e95ac1c522eb55d20c240c7
Montu-Gohain <76866991+Montu-Gohain@users.noreply.github.com>
2022-11-21 14:42:50 +0530

    given a title

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { useEffect } from 'react';
 import './App.css';
 import { LoadingPosts } from './LoadingPosts';
 
 import { PostsUser } from './PostsUser';
 
 function App() {
+
+  useEffect(() => {
+    document.title = "Userlist with Avatars";  
+  }, []);
+
   return (
     <div className="container p-4 bg-rose-700 mt-10">
         <PostsUser />
